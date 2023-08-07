8925354af1232969f8c0fd58324d5bf643174f66
Yash123456789000 <127078654+Yash123456789000@users.noreply.github.com>
2023-08-07 21:49:02 +0530

    smart contract

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import abi from './abi.json';
 import './App.css';
 
 function App() {
+  const [contract, setContract] = useSate();
+  const [todocount, setTodoCount] = useState(0);
+  const [inputItem, setInputItem] = useState();
+  const [inputListItem, setInputListItem] = useState();
+  const [inputListItemRes, setInputListItemRes]= useState();
+
+  const contractExecution = async() => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.contract("0xd9145CCE52D386f254917e481eB44e9943F39138",abi,signer)
+    setContract(contract)
+  }
+
+  const getTodoCount = async () => {
+    if(contract){
+      const res = await contract.count();
+      setTodoCount(Number(res))
+    }
+  }
+
+  useEffect(() => {
+    contractExecution();
+  },[])
+
+  const handleChange = (e)=>{
+    setInputItem(e.target.value)
+  }
+
+  const handleSubmit = async () => {
+    const res = await contract.todoList(inputItem-1);
+  }
+
+  const handleGetTodoList = async () => {
+    const res = await contract.todoList(inputListItem);
+    setInputListItemRes(res);
+  }
+
+  const handleTodoList = (e) => {
+    setInputListItem(e.target.value);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getTodoCount}>Get The Count</button>
+      <h1>Count of todo:-{todoCount}</h1>
+      <div>
+        Enter the input value
+        <input onChange={handleChange}></input>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <div>
+        <input onChange={handleTodoList}></input>
+        <button onClick={handleGetTodoList}>Get Todo List</button>
+        <h3>{inputListItemRes}</h3>
+
+      </div>
     </div>
   );
 }
